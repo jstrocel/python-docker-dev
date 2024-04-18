d424f3455e18508b88beae697d1c0b5e423e19f8
James Strocel <james.strocel@gmail.com>
2024-04-18 14:15:39 -0700

    Changing ping message

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!!!!'
+    return 'Hello, Docker!!!! Today is Thursday April 18!!!!'
 
 
 # @app.route('/widgets')
